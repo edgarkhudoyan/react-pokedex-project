3482d867e7608bf2a07b088ab8f6d34861799565
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-10 18:57:51 +0400

    refactor elements

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -10,7 +10,9 @@ const Pokecard = ({ id, name, type, exp }) => {
   return (
     <div className="Pokecard">
       <h1 className="Pokecard-title">{name}</h1>
-      <img src={imgSrc} alt="" />
+      <div className="Pokecard-image">
+        <img src={imgSrc} alt="" />
+      </div>
       <div className="Pokecard-data"> TYPE: {type}</div>
       <div className="Pokecard-data"> EXP: {exp}</div>
     </div>
