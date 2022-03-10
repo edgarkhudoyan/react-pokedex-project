ea2c3e0885cae5ef03bf0ea96a7a1eceeb1b2919
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-10 04:22:06 +0400

    use map function to render pokecards and add basic styles

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,5 +1,19 @@
-const Pokecard = () => {
-  return <div className="Pokecard"></div>;
+import './Pokecard.css';
+
+const POKE_API =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const Pokecard = ({ id, name, type, exp }) => {
+  let imgSrc = `${POKE_API}${id}.png`;
+
+  return (
+    <div className="Pokecard">
+      <h1 className="Pokecard-title">{name}</h1>
+      <img src={imgSrc} alt="" />
+      <div className="Pokecard-data"> TYPE: {type}</div>
+      <div className="Pokecard-data"> EXP: {exp}</div>
+    </div>
+  );
 };
 
 export default Pokecard;
